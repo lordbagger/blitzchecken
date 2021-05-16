03423dd8dd758eb2b20d551e7fa7c0019ff06181
Lord Bagger <erik.bagger.ext@ausy-technologies.de>
2021-05-16 17:45:16 +0200

    first commit: project structure

---
+++ lib/blitzchecken.ex
@@ -0,0 +1,18 @@
+defmodule Blitzchecken do
+  @moduledoc """
+  Documentation for `Blitzchecken`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blitzchecken.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/blitzchecken_test.exs
@@ -0,0 +1,8 @@
+defmodule BlitzcheckenTest do
+  use ExUnit.Case
+  doctest Blitzchecken
+
+  test "greets the world" do
+    assert Blitzchecken.hello() == :world
+  end
+end
