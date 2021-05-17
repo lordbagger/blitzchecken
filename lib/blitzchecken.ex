ca3989c17492f3a3accff238a3d2d481f3d57f4a
Lord Bagger <erik.bagger.ext@ausy-technologies.de>
2021-05-17 17:47:35 +0200

    Blitzchecken: command-line arguments are read as list

--- lib/blitzchecken.ex
+++ lib/blitzchecken.ex
@@ -1,13 +1,8 @@
 defmodule Blitzchecken do
-  def main(args) when is_binary(args) do
+  def main(args) do
     args
-    |> parse_args
     |> Enum.map(&Task.async(Blitzchecken.Worker, :check_url, [&1]))
     |> Enum.map(&Task.await(&1))
     |> Enum.each(&IO.puts(&1))
-  end
-
-  defp parse_args(args) do
-    args |> String.split()
   end
 end
--- test/blitzchecken_test.exs
+++ test/blitzchecken_test.exs
@@ -5,7 +5,7 @@ defmodule BlitzcheckenTest do
   import Mock
 
   test "given an input argument, prints the correct output in the console" do
-    args = "https://www.ironmaiden.com  http://www.judaspriest.de not-a-url.com"
+    args = ["https://www.ironmaiden.com", "http://www.judaspriest.de", "not-a-url.com"]
 
     expected_output = """
     GET https://www.ironmaiden.com -> 1.0 ms 200
@@ -13,14 +13,16 @@ defmodule BlitzcheckenTest do
     IGNORED not-a-url.com
     """
 
-    with_mock(HTTPoison, [
+    with_mock(HTTPoison,
       get: fn
-        ("https://www.ironmaiden.com") -> {:ok, %HTTPoison.Response{status_code: 200}}
-        ("http://www.judaspriest.de") ->
+        "https://www.ironmaiden.com" ->
+          {:ok, %HTTPoison.Response{status_code: 200}}
+
+        "http://www.judaspriest.de" ->
           Process.sleep(1)
           {:error, %HTTPoison.Error{reason: :nxdomain}}
       end
-    ]) do
+    ) do
       assert capture_io(fn -> Blitzchecken.main(args) end) == expected_output
     end
   end
