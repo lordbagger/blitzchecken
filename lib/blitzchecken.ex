9ed0d99b7ec6ef53fae32e2b0bb5a643f4666fc0
Lord Bagger <erik.bagger.ext@ausy-technologies.de>
2021-05-17 17:28:09 +0200

    Blitzchecken: parses command line arguments and perform async requests. Added tests

--- lib/blitzchecken.ex
+++ lib/blitzchecken.ex
@@ -1,18 +1,13 @@
 defmodule Blitzchecken do
-  @moduledoc """
-  Documentation for `Blitzchecken`.
-  """
+  def main(args) when is_binary(args) do
+    args
+    |> parse_args
+    |> Enum.map(&Task.async(Blitzchecken.Worker, :check_url, [&1]))
+    |> Enum.map(&Task.await(&1))
+    |> Enum.each(&IO.puts(&1))
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Blitzchecken.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp parse_args(args) do
+    args |> String.split()
   end
 end
--- test/blitzchecken_test.exs
+++ test/blitzchecken_test.exs
@@ -1,8 +1,27 @@
 defmodule BlitzcheckenTest do
   use ExUnit.Case
-  doctest Blitzchecken
 
-  test "greets the world" do
-    assert Blitzchecken.hello() == :world
+  import ExUnit.CaptureIO
+  import Mock
+
+  test "given an input argument, prints the correct output in the console" do
+    args = "https://www.ironmaiden.com  http://www.judaspriest.de not-a-url.com"
+
+    expected_output = """
+    GET https://www.ironmaiden.com -> 1.0 ms 200
+    GET http://www.judaspriest.de -> 2.0 ms nxdomain
+    IGNORED not-a-url.com
+    """
+
+    with_mock(HTTPoison, [
+      get: fn
+        ("https://www.ironmaiden.com") -> {:ok, %HTTPoison.Response{status_code: 200}}
+        ("http://www.judaspriest.de") ->
+          Process.sleep(1)
+          {:error, %HTTPoison.Error{reason: :nxdomain}}
+      end
+    ]) do
+      assert capture_io(fn -> Blitzchecken.main(args) end) == expected_output
+    end
   end
 end
