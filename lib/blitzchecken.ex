89c73a3f783b6515e86e466c8995da5ead7f9efd
Lord Bagger <erik.bagger.ext@ausy-technologies.de>
2021-05-17 20:35:39 +0200

    passing :infinity in Task.await

--- lib/blitzchecken.ex
+++ lib/blitzchecken.ex
@@ -2,7 +2,7 @@ defmodule Blitzchecken do
   def main(args) do
     args
     |> Enum.map(&Task.async(Blitzchecken.Worker, :check_url, [&1]))
-    |> Enum.map(&Task.await(&1))
+    |> Enum.map(&Task.await(&1, :infinity))
     |> Enum.each(&IO.puts(&1))
   end
 end
